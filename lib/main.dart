d10e0a1dc6fb5332897cde2105859cb5ac3b5c50
rayhanshuvo1994 <rayhanshuvo.bd@gmail.com>
2020-02-12 12:59:42 +0600

    gfhg

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: Scaffold(
+
+      appBar:AppBar(
+
+        backgroundColor: Colors.red,
+        title: Text('Maksudul alagd'),
+
+
+
+      ),
+      body: Center(
+        child: Image(
+
+     image: AssetImage('assets/a.png'),
+
+
+
+        ),
+
+
+
+
+      ),
+
+
+    ),
+
+
+  ));
+}
+
+/*
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example titlee'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}*/
